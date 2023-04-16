9f5204fe0fb4959264f6145b4d178a94ed022c86
tairitsu86 <benimiku9@gmail.com>
2023-04-16 14:54:10 +0800

    remove the temp file of weather data, it has some problem on path

--- src/main/java/api/concat/demo/getservice/impl/EnglishWeatherServiceImpl.java
+++ src/main/java/api/concat/demo/getservice/impl/EnglishWeatherServiceImpl.java
@@ -19,7 +19,7 @@ public class EnglishWeatherServiceImpl implements WeatherService {
     @Autowired
     private InstantMessagingService instantMessagingService;
 
-    private EnglishWeatherBean todayWeather;
+    private static EnglishWeatherBean todayWeather;
 
     private InstantMessagingService.GroupData groupData;
 
@@ -29,16 +29,12 @@ public class EnglishWeatherServiceImpl implements WeatherService {
 
     @Override
     public String getWeatherData() {
-        if(todayWeather==null) UpdateWeatherByFile();
+        if(todayWeather==null) UpdateWeatherByRequest();
         String weatherData = String.format("%s weather",todayWeather.getAddress());
         EnglishWeatherBean.Day day = todayWeather.getDays().get(0);
         if(!isToday(day.getDatetime())){
             System.out.println("Old weather data bean!");
-            UpdateWeatherByFile();
-            if(!isToday(day.getDatetime())){
-                System.out.println("Old weather data file!");
-                UpdateWeatherByRequest();
-            }
+            UpdateWeatherByRequest();
             return getWeatherData();
         }
         weatherData = String.format("%s%s\n",weatherData,day.getDatetime());
@@ -60,39 +56,39 @@ public class EnglishWeatherServiceImpl implements WeatherService {
         LocalDate date = LocalDate.of(year,month,day);
         return todayDate.isEqual(date);
     }
-    public void UpdateWeatherByFile(){
-        if(this.getClass().getResourceAsStream("/EnglishWeatherJson.json")==null){
-            UpdateWeatherByRequest();
-            return;
-        }
-        StringBuilder fileTemp = new StringBuilder("");
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/EnglishWeatherJson.json"), "UTF-8"));
-            String temp;
-            while((temp=br.readLine())!=null) {
-                fileTemp.append(temp);
-            }
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            todayWeather = objectMapper.readValue(fileTemp.toString(), EnglishWeatherBean.class);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-    }
+//    public void UpdateWeatherByFile(){
+//        if(this.getClass().getResourceAsStream("/EnglishWeatherJson.json")==null){
+//            UpdateWeatherByRequest();
+//            return;
+//        }
+//        StringBuilder fileTemp = new StringBuilder("");
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        try {
+//            BufferedReader br = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/EnglishWeatherJson.json"), "UTF-8"));
+//            String temp;
+//            while((temp=br.readLine())!=null) {
+//                fileTemp.append(temp);
+//            }
+//        } catch (UnsupportedEncodingException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        try {
+//            todayWeather = objectMapper.readValue(fileTemp.toString(), EnglishWeatherBean.class);
+//        } catch (JsonProcessingException e) {
+//            e.printStackTrace();
+//        }
+//    }
     public void UpdateWeatherByRequest(){
         RestTemplate restTemplate = new RestTemplate();
-        ObjectMapper objectMapper = new ObjectMapper();
+//        ObjectMapper objectMapper = new ObjectMapper();
         todayWeather = restTemplate.getForObject("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/taiwan?unitGroup=metric&key=HCMGZY5UNQL5C877RCRH4ZZAP&contentType=json", EnglishWeatherBean.class);
-        File file = new File("./src/main/resources/EnglishWeatherJson.json");
-        try (FileWriter fileWriter = new FileWriter(file);){
-            fileWriter.write(objectMapper.writeValueAsString(todayWeather));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+//        File file = new File("./src/main/resources/EnglishWeatherJson.json");
+//        try (FileWriter fileWriter = new FileWriter(file);){
+//            fileWriter.write(objectMapper.writeValueAsString(todayWeather));
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
