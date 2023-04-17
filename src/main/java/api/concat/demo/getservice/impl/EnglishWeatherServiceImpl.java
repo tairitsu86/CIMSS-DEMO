4e462fddc7112eb45d4754764b9d1b6fe478c147
tairitsu86 <benimiku9@gmail.com>
2023-04-17 08:56:00 +0800

    update time zone, now use utc+8(was system time zone)

--- src/main/java/api/concat/demo/getservice/impl/EnglishWeatherServiceImpl.java
+++ src/main/java/api/concat/demo/getservice/impl/EnglishWeatherServiceImpl.java
@@ -4,15 +4,13 @@ import api.concat.demo.getservice.InstantMessagingService;
 import api.concat.demo.getservice.WeatherService;
 import api.concat.demo.getservice.jsonBean.EnglishWeatherBean;
 import api.concat.demo.getservice.jsonBean.EventBean;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-
-import java.io.*;
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @Service("EN-Weather")
 public class EnglishWeatherServiceImpl implements WeatherService {
@@ -50,45 +48,17 @@ public class EnglishWeatherServiceImpl implements WeatherService {
             instantMessagingService.replyMessage(groupData,event,getWeatherData());
     }
     public boolean isToday(String datetime){
-        LocalDate todayDate = LocalDate.now();
+        ZonedDateTime utc8Now = ZonedDateTime.now(ZoneId.of("UTC+8"));
+        System.out.printf("now is %s!\n",utc8Now.toString());
+        LocalDate todayDate = utc8Now.toLocalDate();
         String temp[] = datetime.split("-");
         int year = Integer.parseInt(temp[0]),month = Integer.parseInt(temp[1]),day = Integer.parseInt(temp[2]);
         LocalDate date = LocalDate.of(year,month,day);
         return todayDate.isEqual(date);
     }
-//    public void UpdateWeatherByFile(){
-//        if(this.getClass().getResourceAsStream("/EnglishWeatherJson.json")==null){
-//            UpdateWeatherByRequest();
-//            return;
-//        }
-//        StringBuilder fileTemp = new StringBuilder("");
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        try {
-//            BufferedReader br = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/EnglishWeatherJson.json"), "UTF-8"));
-//            String temp;
-//            while((temp=br.readLine())!=null) {
-//                fileTemp.append(temp);
-//            }
-//        } catch (UnsupportedEncodingException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//        try {
-//            todayWeather = objectMapper.readValue(fileTemp.toString(), EnglishWeatherBean.class);
-//        } catch (JsonProcessingException e) {
-//            e.printStackTrace();
-//        }
-//    }
+
     public void UpdateWeatherByRequest(){
         RestTemplate restTemplate = new RestTemplate();
-//        ObjectMapper objectMapper = new ObjectMapper();
         todayWeather = restTemplate.getForObject("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/taiwan?unitGroup=metric&key=HCMGZY5UNQL5C877RCRH4ZZAP&contentType=json", EnglishWeatherBean.class);
-//        File file = new File("./src/main/resources/EnglishWeatherJson.json");
-//        try (FileWriter fileWriter = new FileWriter(file);){
-//            fileWriter.write(objectMapper.writeValueAsString(todayWeather));
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
     }
 }
